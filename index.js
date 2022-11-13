5cbf4d68226063b70728fee406e7e0f18286984c
caulok <ramicaulo@gmail.com>
2022-11-13 13:07:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const cumple = new Date('january 12 1997');
+const libro = {
+    titulo: '1984',
+    autor: 'George Orwell',
+    fecha: '1947',
+    url: 'https://www.philosophia.cl/biblioteca/orwell/1984.pdf'
+}
+const datos = ["Ramiro", 25, false, cumple, libro];
